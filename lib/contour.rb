9cc26f874b78c68e44ad2b3c02a444ba5a0ea012
Remo Mueller <remosm@gmail.com>
2011-08-31 16:00:33 -0400

    Menu formatting added.

--- lib/contour.rb
+++ lib/contour.rb
@@ -18,6 +18,23 @@ module Contour
   mattr_accessor :header_title_image
   @@header_title_image = nil
   
+  # Default Menu Items
+  mattr_accessor :menu_items
+  @@menu_items = [
+    {
+      :name => 'Login', :id => 'auth', :display => 'not_signed_in', :position => 'right', :position_class => 'right',
+      :links => [{:name => 'Login', :path => 'new_user_session_path'}, {:html => "<hr>"}, {:name => 'Sign Up', :path => 'new_user_registration_path'}]
+    },
+    {
+      :name => 'current_user.name', :eval => true, :id => 'auth', :display => 'signed_in', :position => 'right', :position_class => 'right',
+      :links => [{:html => '"<div style=\"white-space:nowrap\">"+current_user.name+"</div>"', :eval => true}, {:html => '"<div class=\"small quiet\">"+current_user.email+"</div>"', :eval => true}, {:name => 'Authentications', :path => 'authentications_path'}, {:html => "<hr>"}, {:name => 'Logout', :path => 'destroy_user_session_path'}]
+    },
+    {
+      :name => 'Home', :id => 'home', :display => 'always', :position => 'left', :position_class => 'left',
+      :links => [{:name => 'Home', :path => 'root_path'}, {:html => "<hr>"}, {:name => 'About', :path => 'about_path'}]
+    }
+  ]
+    
   def self.setup
     yield self
   end
--- lib/generators/templates/contour.rb
+++ lib/generators/templates/contour.rb
@@ -13,4 +13,18 @@ Contour.setup do |config|
   # Enter your application header title image here.
   # config.header_title_image = ''
   
+  # Enter the items you wish to see in the menu
+  # config.menu_items = [{
+  #   :name => 'Login', :id => 'auth', :display => 'not_signed_in', :position => 'right', :position_class => 'right',
+  #   :links => [{:name => 'Login', :path => 'new_user_session_path'}, {:html => "<hr>"}, {:name => 'Sign Up', :path => 'new_user_registration_path'}]
+  # },
+  # {
+  #   :name => 'current_user.name', :eval => true, :id => 'auth', :display => 'signed_in', :position => 'right', :position_class => 'right',
+  #   :links => [{:html => '"<div style=\"white-space:nowrap\">"+current_user.name+"</div>"', :eval => true}, {:html => '"<div class=\"small quiet\">"+current_user.email+"</div>"', :eval => true}, {:name => 'Authentications', :path => 'authentications_path'}, {:html => "<hr>"}, {:name => 'Logout', :path => 'destroy_user_session_path'}]
+  # },
+  # {
+  #   :name => 'Home', :id => 'home', :display => 'always', :position => 'left', :position_class => 'left',
+  #   :links => [{:name => 'Home', :path => 'root_path'}, {:html => "<hr>"}, {:name => 'About', :path => 'about_path'}]
+  # }]
+  
 end
