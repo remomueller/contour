5aa1565984bdc58e1b387b2e4637ed1b4d27e73a
Remo Mueller <remosm@gmail.com>
2011-09-05 15:03:16 -0400

    Menu items are now rendered using html_safe
    Links and menu items can now have a :condition parameter passed to it which is a string that can be evaluated to true or false
    	ex: :condition => 'current_user.admin?'

--- lib/generators/templates/contour.rb
+++ lib/generators/templates/contour.rb
@@ -15,15 +15,15 @@ Contour.setup do |config|
   
   # Enter the items you wish to see in the menu
   # config.menu_items = [{
-  #   :name => 'Login', :id => 'auth', :display => 'not_signed_in', :position => 'right', :position_class => 'right',
+  #   :name => 'Login', :id => 'auth', :display => 'not_signed_in', :position => 'right', :position_class => 'right', :condition => 'true',
   #   :links => [{:name => 'Login', :path => 'new_user_session_path'}, {:html => "<hr>"}, {:name => 'Sign Up', :path => 'new_user_registration_path'}]
   # },
   # {
-  #   :name => 'current_user.name', :eval => true, :id => 'auth', :display => 'signed_in', :position => 'right', :position_class => 'right',
+  #   :name => 'current_user.name', :eval => true, :id => 'auth', :display => 'signed_in', :position => 'right', :position_class => 'right', :condition => 'true',
   #   :links => [{:html => '"<div style=\"white-space:nowrap\">"+current_user.name+"</div>"', :eval => true}, {:html => '"<div class=\"small quiet\">"+current_user.email+"</div>"', :eval => true}, {:name => 'Authentications', :path => 'authentications_path'}, {:html => "<hr>"}, {:name => 'Logout', :path => 'destroy_user_session_path'}]
   # },
   # {
-  #   :name => 'Home', :id => 'home', :display => 'always', :position => 'left', :position_class => 'left',
+  #   :name => 'Home', :id => 'home', :display => 'always', :position => 'left', :position_class => 'left', :condition => 'true',
   #   :links => [{:name => 'Home', :path => 'root_path'}, {:html => "<hr>"}, {:name => 'About', :path => 'about_path'}]
   # }]
   
