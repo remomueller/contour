8e62d19090d8379fcd9231dd49ed1b1e0cf215b8
Remo Mueller <remosm@gmail.com>
2011-09-06 15:12:41 -0400

    Updated registrations controller.

--- app/controllers/contour/registrations_controller.rb
+++ app/controllers/contour/registrations_controller.rb
@@ -1,9 +1,24 @@
 class Contour::RegistrationsController < Devise::RegistrationsController
-  def create
-    super
-    session[:omniauth] = nil unless @user.new_record?
-  end
+  prepend_before_filter :require_no_authentication, :only => [ :new ]
   
+  def create
+    if user_signed_in?
+      params[:user][:password] = params[:user][:password_confirmation] = Digest::SHA1.hexdigest(Time.now.usec.to_s)[0..19]
+      @user = User.new(params[:user])
+      if @user.save
+        [:pp_committee, :pp_committee_secretary, :steering_committee, :steering_committee_secretary, :system_admin, :status].each do |attribute|
+          @user.update_attribute attribute, params[:user][attribute]
+        end
+        redirect_to(@user, :notice => 'User was successfully created.')
+      else
+        render :action => "/users/new"
+      end
+    else
+      super
+      session[:omniauth] = nil unless @user.new_record?
+    end
+  end
+    
   private
   
   def build_resource(*args)
