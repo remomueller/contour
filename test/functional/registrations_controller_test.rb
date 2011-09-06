bc8ba5f6f74e5dc9814b1aae03d81f4dcbb2d3de
Remo Mueller <remosm@gmail.com>
2011-09-06 16:00:01 -0400

    Target allowed as a parameter for a link.

--- test/functional/registrations_controller_test.rb
+++ test/functional/registrations_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class RegistrationsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    login(users(:admin))
+    request.env["devise.mapping"] = Devise.mappings[:user]
+  end
+  
+  test "an admin should be able to create new user" do
+    assert_difference('User.count') do
+      post :create, :user => {:first_name => 'First Name', :last_name => 'Last Name', :status => 'active', :email => 'new_registration@example.com',
+        :steering_committee => true, :steering_committee_secretary => false, :pp_committee => false, :pp_committee_secretary => false, :system_admin => false}
+    end
+  
+    assert_redirected_to user_path(assigns(:user))
   end
 end
