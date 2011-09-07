477c925594df8ce4b570c165bde04e935b72ce89
Remo Mueller <remosm@gmail.com>
2011-09-07 14:36:06 -0400

    Integrating the devise install with contour install.  Removing duplicate files.

--- lib/generators/contour/install_generator.rb
+++ lib/generators/contour/install_generator.rb
@@ -10,9 +10,9 @@ module Contour
         template "contour.rb", "config/initializers/contour.rb"
       end
 
-      def copy_devise
-        template "devise.rb", "config/initializers/devise.rb"
-      end
+      # def copy_devise
+      #   template "devise.rb", "config/initializers/devise.rb"
+      # end
 
       def copy_omniauth
         template "omniauth.rb", "config/initializers/omniauth.rb"
@@ -32,9 +32,22 @@ module Contour
         contour_routes << "match '/auth/:provider/callback' => 'contour/authentications#create'"
         contour_routes << "match '/auth/:provider' => 'contour/authentications#passthru'"
         contour_routes << "resources :authentications, :controller => 'contour/authentications'"
-        contour_routes << "devise_for :users, :controllers => {:registrations => 'contour/registrations', :sessions => 'contour/sessions', :passwords => 'contour/passwords'}, :path_names => { :sign_up => 'register', :sign_in => 'login' }"
-        # route "root :to => 'welcome'"
-        route contour_routes.join("\n")
+        # contour_routes << "devise_for :users, :controllers => {:registrations => 'contour/registrations', :sessions => 'contour/sessions', :passwords => 'contour/passwords'}, :path_names => { :sign_up => 'register', :sign_in => 'login' }"
+        contour_routes.reverse.each do |contour_route|
+          route contour_route
+        end
+      end
+      
+      def install_devise
+        generate("devise:install")
+        # model_name = ask("What would you like the user model to be called? [user]")
+        # model_name = "user" if model_name.blank?
+        model_name = "user"
+        generate("devise", model_name)
+      end
+
+      def show_readme
+        readme "README" if behavior == :invoke
       end
       
       # def copy_locale
