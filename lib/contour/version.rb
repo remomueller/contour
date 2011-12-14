85dfef8c61029ce4af493cf68a8f40e2c9b06b68
Remo Mueller <remosm@gmail.com>
2011-12-14 10:49:05 -0500

    Version 0.7.1 finalized

--- lib/contour/version.rb
+++ lib/contour/version.rb
@@ -3,7 +3,7 @@ module Contour
     MAJOR = 0
     MINOR = 7
     TINY = 1
-    BUILD = "pre" # nil, "pre", "rc", "rc2"
+    BUILD = nil # nil, "pre", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
