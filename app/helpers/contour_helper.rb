fe4da3f5637e0972bf8b1be1fc2de312e91afea1
Remo Mueller <remosm@gmail.com>
2012-05-02 11:44:17 -0400

    Flash messages now only show specific types of flash messages, instead of everything that's added to the flash hash

--- app/helpers/contour_helper.rb
+++ app/helpers/contour_helper.rb
@@ -3,16 +3,17 @@ module ContourHelper
   def flash_block
     output = ''
     flash.each do |type, message|
-      output += flash_container(type, message)
+      output += flash_container(type, message) if ['alert', 'notice', 'error', 'warning', 'success', 'info'].include?(type.to_s)
     end
 
     raw(output)
   end
 
   def flash_container(type, message)
-    raw(content_tag(:div, :class => "alert alert-#{type}") do
-      content_tag(:a, raw("&times;"),:class => 'close', :data => {:dismiss => 'alert'}) +
-      message
-    end)
+    type = 'success' if type.to_s == 'notice'
+    type = 'error' if type.to_s == 'alert'
+    content_tag(:div, class: "alert alert-#{type}") do
+      content_tag(:a, raw("&times;"), href: '#', class: 'close', data: { dismiss: 'alert' }) + message
+    end.html_safe
   end
 end
