917f2c04f549813d430ccd6fed2b50daae30b631
Remo Mueller <remosm@gmail.com>
2012-05-03 16:05:20 -0400

    Redirects to the user's alternate login page if a user fails to login on the default devise sign in page

--- app/controllers/contour/sessions_controller.rb
+++ app/controllers/contour/sessions_controller.rb
@@ -19,4 +19,26 @@ class Contour::SessionsController < Devise::SessionsController
     super
   end
 
+  # Overwrite Devise authentication to check if the user is typing another credential into the default box
+  # if so, find alternative login methods for that user and forward the user to those login screens
+  def create
+    # resource = warden.authenticate!(auth_options)
+    resource = warden.authenticate(auth_options)
+
+    if resource
+      set_flash_message(:notice, :signed_in) if is_navigational_format?
+      sign_in(resource_name, resource)
+      respond_with resource, location: after_sign_in_path_for(resource)
+    else
+      resource = resource_name.to_s.titleize.constantize.find_by_email(params[resource_name][:email])
+      if resource and resource.respond_to?('authentications') and providers = resource.authentications.pluck(:provider).uniq and providers.size > 0
+        redirect_to request.script_name + '/auth/' + providers.first
+      elsif providers = Authentication.where(uid: params[resource_name][:email]).pluck(:provider).uniq and providers.size > 0
+        redirect_to request.script_name + '/auth/' + providers.first
+      else
+        resource = warden.authenticate!(auth_options)
+      end
+    end
+  end
+
 end
