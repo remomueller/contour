e6663509ea9bdd5280189f071c9ed43a680f3cdd
Remo Mueller <remosm@gmail.com>
2012-05-07 15:28:58 -0400

    Add dynamic scrollable modal boxes

--- app/assets/javascripts/contour.js
+++ app/assets/javascripts/contour.js
@@ -4,10 +4,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require twitter-bootstrap/v2.0.3/bootstrap
-//= require twitter-bootstrap/bootstrap-collapse.js
-//= require twitter-bootstrap/bootstrap-dropdown.js
-//= require twitter-bootstrap/bootstrap-modal.js
-//= require twitter-bootstrap/bootstrap-tab.js
+//= require twitter-bootstrap/bootstrap-collapse
+//= require twitter-bootstrap/bootstrap-dropdown
+//= require twitter-bootstrap/bootstrap-modal
+//= require twitter-bootstrap/bootstrap-tab
+//= require twitter-bootstrap/bootstrap-scroll-modal
 //= require twitter-bootstrap/bootstrap
 //= require external/highcharts-2.1.9.src.js
 //= require external/exporting-2.1.9.src.js
