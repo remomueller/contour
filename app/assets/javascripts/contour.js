e3f0737632f01884eb42ced510f978a013524b4c
Remo Mueller <remosm@gmail.com>
2012-06-19 11:01:41 -0400

    Twitter Bootstrap updated to 2.0.4
    Now includes all Bootstrap JavaScript Plugins
    
    Read more about them here:
    http://twitter.github.com/bootstrap/javascript.html

--- app/assets/javascripts/contour.js
+++ app/assets/javascripts/contour.js
@@ -3,13 +3,24 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require twitter-bootstrap/v2.0.3/bootstrap
-//= require twitter-bootstrap/bootstrap-collapse
-//= require twitter-bootstrap/bootstrap-dropdown
-//= require twitter-bootstrap/bootstrap-modal
-//= require twitter-bootstrap/bootstrap-tab
+//
+//= require twitter-bootstrap/v2.0.4/bootstrap
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-alert
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-button
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-carousel
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-collapse
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-dropdown
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-modal
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-popover
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-scrollspy
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-tab
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-tooltip
+//= require twitter-bootstrap/v2.0.4/plugins/bootstrap-typeahead
+//
 //= require twitter-bootstrap/bootstrap-scroll-modal
 //= require twitter-bootstrap/bootstrap
+//
 //= require external/highcharts-2.1.9.src.js
 //= require external/exporting-2.1.9.src.js
+//
 //= require_tree .
