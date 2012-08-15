a3a67b9fffa72af3e79f0c8155fe9628350c7056
Remo Mueller <remosm@gmail.com>
2012-08-15 09:19:48 -0400

    Version bump to 1.1.0.pre

--- lib/contour/version.rb
+++ lib/contour/version.rb
@@ -1,9 +1,9 @@
 module Contour
   module VERSION #:nodoc:
     MAJOR = 1
-    MINOR = 0
-    TINY = 4
-    BUILD = nil # nil, "pre", "rc", "rc2"
+    MINOR = 1
+    TINY = 0
+    BUILD = "pre" # nil, "pre", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
