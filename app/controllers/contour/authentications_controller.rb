2a69f93057b64d98343e404c498f5e237880e846
Remo Mueller <remosm@gmail.com>
2013-02-26 11:29:32 -0500

    Use skip_before_action instead of skip_before_filter

--- app/controllers/contour/authentications_controller.rb
+++ app/controllers/contour/authentications_controller.rb
@@ -1,5 +1,5 @@
 class Contour::AuthenticationsController < ApplicationController
-  skip_before_filter :verify_authenticity_token, only: :create
+  skip_before_action :verify_authenticity_token, only: :create
 
   def index
     @authentications = current_user.authentications if current_user
