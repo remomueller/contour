46cc0967de15b17623234941a0531b79e8081f90
Remo Mueller <remosm@gmail.com>
2013-09-03 13:08:15 -0400

    Updated errors to use BS3 has-error class

--- lib/contour/engine.rb
+++ lib/contour/engine.rb
@@ -6,7 +6,7 @@ module Contour
     engine_name :contour
 
     # Overwrite Rails errors to use Twitter/Contour CSS classes
-    config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<span class=\"control-group-error\">#{html_tag}</span>".html_safe }
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<span class=\"has-error\">#{html_tag}</span>".html_safe }
 
   end
 end
