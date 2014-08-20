8305df36b4d9ff1d348594f95555290b7af3a55f
Remo Mueller <remosm@gmail.com>
2014-08-20 14:31:06 -0400

    Font family asset path now fixed with sass-rails patch

--- lib/contour/fixes.rb
+++ lib/contour/fixes.rb
@@ -1,6 +1,7 @@
 require 'contour/fixes/devise.rb'
 require 'contour/fixes/omniauth.rb'
 require 'contour/fixes/rack.rb'
+require 'contour/fixes/sass-rails.rb'
 
 module Contour
   module Fixes
---
+++ lib/contour/fixes/sass-rails.rb
@@ -0,0 +1,8 @@
+require 'sass/rails'
+
+class Sass::Rails::Importer
+  def sass_file? filename
+    filename = filename.to_s
+    SASS_EXTENSIONS.keys.any?{|ext| filename.match /#{ext}$/}
+  end
+end
