742a1b94ac8ebf1c27bd92ae062f20b4aa84e792
Remo Mueller <remosm@gmail.com>
2015-06-08 12:25:05 -0400

    Version bump to 3.0.0.rc

--- lib/contour/version.rb
+++ lib/contour/version.rb
@@ -3,7 +3,7 @@ module Contour
     MAJOR = 3
     MINOR = 0
     TINY = 0
-    BUILD = "beta1" # nil, "pre", "rc", "rc2"
+    BUILD = "rc" # nil, "pre", "rc", "rc2"
 
     STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
   end
