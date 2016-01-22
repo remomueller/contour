9f65f8ef471d081579a423a35d2c65f8938673af
Remo Mueller <remosm@gmail.com>
2016-01-22 10:22:13 -0500

    Version bump to 3.1.0.pre

--- lib/contour/version.rb
+++ lib/contour/version.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 module Contour
   module VERSION #:nodoc:
     MAJOR = 3
-    MINOR = 0
-    TINY = 1
-    BUILD = nil # nil, "pre", "rc", "rc2"
+    MINOR = 1
+    TINY = 0
+    BUILD = 'pre'.freeze # 'pre', 'rc', 'rc2', nil
 
-    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.')
+    STRING = [MAJOR, MINOR, TINY, BUILD].compact.join('.').freeze
   end
 end
